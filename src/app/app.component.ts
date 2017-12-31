681f8a9473071ae6327ed01eef8c9a931914647a
Benidal <a.benidal@gmail.com>
2017-12-31 20:48:51 +0000

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  description: string = '';
+  name: string = '';
+  titleAlert:string = 'This field is required';
+
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(500)])],
+      'validate': ''
+    }); 
+  }
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
 }
